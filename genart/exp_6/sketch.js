e4c02ea1db6a8374a3843b91a449302fb04b987e
silmood <dsilmood@gmail.com>
2021-01-05 23:40:33 -0700

    Add vortex rotation

--- genart/exp_6/sketch.js
+++ genart/exp_6/sketch.js
@@ -13,13 +13,14 @@ function draw() {
 function createVortex() {
   let deepness = 10
   let minSize = 100
+  let vortexSize = width + 200
   let sizeNoise = random(10)
   let offsetMultiplierX = 0.5
   let offsetMultiplierY = 0.5
 
   let layers = _.times(deepness, layer => {
     return {
-      size: map(layer, 0, deepness, minSize, width),
+      size: map(layer, 0, deepness, minSize, vortexSize),
       offsetX: (width - layer.size) * offsetMultiplierX,
       offsetY: (height - layer.size) * offsetMultiplierY
     }
@@ -27,6 +28,7 @@ function createVortex() {
 
   return {
     originalMinSize: minSize,
+    size: vortexSize,
     minSize,
     deepness,
     layers,
@@ -42,72 +44,93 @@ function drawVortex(vortex) {
 
   _.forEach(vortex.layers, drawLayer)
   drawGrid(vortex)
-  drawLayer({ 
-    size: vortex.minSize, 
-    offsetX: (width - vortex.minSize) * vortex.offsetMultiplierX,
-    offsetY: (width - vortex.minSize) * vortex.offsetMultiplierY
-  })
+  //drawLayer({ 
+  //  size: vortex.minSize, 
+  //  offsetX: (width - vortex.minSize) * vortex.offsetMultiplierX + vortex.minSize / 2,
+  //  offsetY: (width - vortex.minSize) * vortex.offsetMultiplierY + vortex.minSize / 2
+  //})
 
 }
 
 function updateVortex(vortex) {
-  vortex.deepness = 10 + (sin(frameCount * PI / 360) * 5)
   updateMinSize(vortex)
-  //updateDividerOffset(vortex)
+  updateDividerOffset(vortex)
   updateLayers(vortex)
 }
 
 function updateMinSize(vortex) {
-  vortex.sizeNoise += 1.5
   vortex.minSize = vortex.originalMinSize + (sin(frameCount * PI / 360) * (vortex.originalMinSize / 2))
 }
 
 
 function updateDividerOffset(vortex) {
-  let x = cos(3 * frameCount * PI / 360)
-  let y = sin(1 * frameCount * PI / 360)
+  let x = cos(1 * frameCount * PI / 360)
+  let y = sin(4 * frameCount * PI / 720)
 
-  vortex.offsetMultiplierX = map(x, -1, 1, 0.1, 0.9)
-  vortex.offsetMultiplierY = map(y, -1, 1, 0.1, 0.9)
+  vortex.offsetMultiplierX = 0.5 //map(x, -1, 1, 0.3, 0.6)
+  vortex.offsetMultiplierY = map(y, -1, 1, 0.4, 0.6)
 }
 
 function updateLayers(vortex) {
+  let maxSize = 0
   _.forEach(vortex.layers, layer => { 
-    layer.size = layer.size >= width ? vortex.minSize : layer.size + 1
-    layer.offsetX = (width - layer.size) * vortex.offsetMultiplierX
-    layer.offsetY = (width - layer.size) * vortex.offsetMultiplierY
+    layer.size = layer.size >= vortex.size ? vortex.minSize : layer.size + 1
+    maxSize = layer.size > maxSize ? layer.size : maxSize
+    layer.offsetX = (width - layer.size) * vortex.offsetMultiplierX + layer.size / 2
+    layer.offsetY = (width - layer.size) * vortex.offsetMultiplierY + layer.size / 2
   })
+
+  vortex.maxSize = maxSize
 }
 
 function drawGrid(vortex) {
   push()
-  translate(width / 2, height / 2)
+  let gridOriginX = ((width - vortex.minSize) * vortex.offsetMultiplierX) + vortex.minSize / 2
+  let gridOriginY = ((height - vortex.minSize) * vortex.offsetMultiplierY) + vortex.minSize / 2
+
+  translate(
+    gridOriginX,
+    gridOriginY
+  )
 
   let c1 = color(249, 9, 137)
   let c2 = color(29, 9, 242)
   blendMode(ADD)
-  strokeWeight(2)
+  strokeWeight(0.15)
 
-  let gridLines = 10 + (sin(frameCount * PI / 360) * 10 / 2)
+  let gridLines = 10 + (sin(frameCount * PI / 720) * 10 / 2)
   let vertexCount = 4
   let vertexTheta = TWO_PI / vertexCount
 
   _.times(vertexCount, v => {
     v += 1
-    let offset = (HALF_PI) * (v - 1) + (PI / vertexCount)
+    let offset = (TWO_PI / vertexCount) * (v - 1) + (PI / vertexCount) 
+
     let theta = (vertexTheta / gridLines)
 
     _.times(gridLines + 1, (l) => {
+
       let x = cos((l * theta) + offset)
       let y = sin((l * theta) + offset)
-      let m = max(abs(x), abs(y))
-      let xMin = ((vortex.minSize / 2) * x) / m
-      let yMin = ((vortex.minSize / 2)* y) / m
-      let xMax = ((width / 2) * x) / m
-      let yMax = ((height / 2) * y) / m
 
+      let xM = cos((l * theta) + (offset + (frameCount * (PI / 360) )))
+      let yM = sin((l * theta) + (offset + (frameCount * (PI / 360) )))
+
+      let m = max(abs(x), abs(y))
+      let xMin = ((vortex.minSize / 2) * xM) / m
+      let yMin = ((vortex.minSize / 2) * yM) / m
+      let xMax = (((vortex.size / 2) * x) / m) + (width / 2 - gridOriginX)
+      let yMax = (((vortex.size / 2) * y) / m) + (height / 2 - gridOriginY)
+
+      strokeWeight(0.5)
       stroke(c1)
       line(xMin, yMin, xMax, yMax)
+      stroke(c2)
+      line(xMin - 2, yMin - 2, xMax - 2, yMax - 2)
+
+      stroke(255)
+      strokeWeight(1)
+      point(xMin, yMin)
     })
 
   })
@@ -118,19 +141,22 @@ function drawGrid(vortex) {
 function drawLayer(layer) {
   push()
   translate(layer.offsetX, layer.offsetY)
+  rotate(frameCount * (PI / 360))
+  rectMode(CENTER)
+  ellipseMode(CENTER)
 
   noFill()
   blendMode(ADD)
 
-  strokeWeight(1)
-  stroke(0)
+  strokeWeight(0.5)
+  stroke(255)
   rect(0, 0, layer.size, layer.size)
   stroke(255, 91, 233)
 
-  strokeWeight(2)
-  rect(-1, -1, layer.size + 2, layer.size + 2)
+  strokeWeight(0.5)
+  rect(0, 0, layer.size + 2, layer.size + 2)
   stroke(0, 238, 24)
-  rect(1, 1, layer.size - 2, layer.size - 2)
+  rect(0, 0, layer.size - 2, layer.size - 2)
 
   pop()
 }
