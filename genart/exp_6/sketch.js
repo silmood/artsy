f9c5871c580bb8c334258787412163db54f56fde
silmood <dsilmood@gmail.com>
2021-01-07 18:07:35 -0700

    🚀 Minimize vortex

--- genart/exp_6/sketch.js
+++ genart/exp_6/sketch.js
@@ -1,19 +1,60 @@
 let vortexA
+let theta = 0
+let capturing = false
+let renderer
+let animationSpeed = 180
+let minLayer, maxLayer
+
 
 function setup() {
-  createCanvas(512, 512)
+  renderer = createCanvas(300, 300)
+  capturer = new CCapture( { format: 'png', framerate: 30, verbose: true, display: false, quality: 30 } )
+  frameRate(30)
+
   vortexA = createVortex()
+
+  minLayer = {
+    size: vortexA.minSize, 
+    offsetX: (width - vortexA.minSize) * vortexA.offsetMultiplierX + vortexA.minSize / 2 + vortexA.offset.x,
+    offsetY: (width - vortexA.minSize) * vortexA.offsetMultiplierY + vortexA.minSize / 2 + vortexA.offset.y,
+    cR: color(241, 56, 244),
+    cB: color(0, 242, 255)
+  }
+
+  maxLayer = {
+    size: vortexA.size, 
+    offsetX: (width - vortexA.minSize) * vortexA.offsetMultiplierX + vortexA.minSize / 2 + vortexA.offset.x,
+    offsetY: (width - vortexA.minSize) * vortexA.offsetMultiplierY + vortexA.minSize / 2 + vortexA.offset.y,
+    cR: color(241, 56, 244),
+    cB: color(0, 242, 255)
+  }
+
 }
 
 function draw() {
   background(0)
   drawVortex(vortexA)
+
+  if (theta >= (PI) && !capturing) {
+    capturer.start()
+    capturer.capture(renderer.canvas)
+    capturing = true
+  } else if (theta >= (TWO_PI) && capturing) {
+    noLoop()
+    capturer.stop()
+    capturer.save()
+    capturing = false
+  }
+
+  capturer.capture(renderer.canvas)
+  theta = frameCount * (PI / (animationSpeed * 2))
 }
 
 function createVortex(offset = createVector(0, 0)) {
   let deepness = 10
-  let minSize = 100
-  let vortexSize = width
+  let vortexSize = 5 * (width / 9)
+  let minSize = vortexSize / 2
+  let offsetMinSize = 3 * minSize / 7
   let sizeNoise = random(10)
   let offsetMultiplierX = 0.5
   let offsetMultiplierY = 0.5
@@ -23,7 +64,7 @@ function createVortex(offset = createVector(0, 0)) {
 
   let layers = _.times(deepness, layer => {
     return {
-      size: map(layer, 0, deepness, minSize, vortexSize),
+      size: map(layer, 0, deepness, minSize - offsetMinSize, vortexSize),
       offsetX: (width - layer.size) * offsetMultiplierX,
       offsetY: (height - layer.size) * offsetMultiplierY,
       cR: color(241, 56, 244),
@@ -37,8 +78,8 @@ function createVortex(offset = createVector(0, 0)) {
     let t = random(360) * starsTheta
     let r = random(minSize, vortexSize)
     return {
-      lightR: random(10),
-      lightB: random(10),
+      lightR: random(100),
+      lightB: random(100),
       cR: color(241, 56, 244),
       cB: color(0, 242, 255),
       theta: t,
@@ -50,7 +91,8 @@ function createVortex(offset = createVector(0, 0)) {
   return {
     originalMinSize: minSize,
     size: vortexSize,
-    minSize,
+    minSize: minSize - offsetMinSize,
+    offsetMinSize,
     deepness,
     layers,
     stars,
@@ -69,61 +111,62 @@ function drawVortex(vortex) {
   updateVortex(vortex)
 
   _.forEach(vortex.layers, (l) => drawLayer(vortex, l))
+
   drawGrid(vortex)
   drawStars(vortex)
-  drawLayer(vortex, { 
-    size: vortex.minSize, 
-    offsetX: (width - vortex.minSize) * vortex.offsetMultiplierX + vortex.minSize / 2 + vortex.offset.x,
-    offsetY: (width - vortex.minSize) * vortex.offsetMultiplierY + vortex.minSize / 2 + vortex.offset.y,
-    cR: color(255, 0, 19),
-    cB: color(0, 242, 255),
-  })
+
+  drawLayer(vortex, minLayer)
+  drawLayer(vortex, maxLayer)
 
 }
 
 function updateVortex(vortex) {
   updateMinSize(vortex)
   updateSpeed(vortex)
-  updateDividerOffset(vortex)
+  //updateDividerOffset(vortex)
   updateLayers(vortex)
   updateStars(vortex)
 }
 
 function updateMinSize(vortex) {
-  vortex.minSize = vortex.originalMinSize + (sin(frameCount * PI / 360) * (vortex.minSize / 2))
+  vortex.minSize = vortex.originalMinSize + (sin(frameCount * PI / animationSpeed) * vortex.offsetMinSize)
+  minLayer.size = vortex.minSize
 }
 
 function updateSpeed(vortex) {
-  vortex.speed = map(vortex.minSize, vortex.originalMinSize - (vortex.originalMinSize / 2), vortex.originalMinSize + (vortex.originalMinSize / 2), 1, 3)
+  vortex.speed = map(vortex.minSize,
+    vortex.originalMinSize - vortex.offsetMinSize,
+    vortex.originalMinSize + vortex.offsetMinSize,
+      1,
+      2
+    ) 
 }
 
 
 function updateDividerOffset(vortex) {
-  let x = cos(3 * frameCount * PI / 720)
-  let y = sin(2 * frameCount * PI / 360)
+  let x = cos(2 * frameCount * PI / animationSpeed)
+  let y = sin(0 * frameCount * PI / (animationSpeed / 2))
 
-  vortex.offsetMultiplierX = map(x, -1, 1, 0.1, 0.9)
+  vortex.offsetMultiplierX = map(x, -1, 1, 0.3, 0.7)
   vortex.offsetMultiplierY = map(y, -1, 1, 0.3, 0.7)
 }
 
 function updateLayers(vortex) {
-  let maxSize = 0
   _.forEach(vortex.layers, layer => { 
-    layer.size = layer.size >= vortex.size ? vortex.minSize : layer.size + vortex.speed
-    maxSize = layer.size > maxSize ? layer.size : maxSize
+    layer.size = layer.size >= vortex.size  ? (vortex.originalMinSize - vortex.offsetMinSize)  : layer.size + vortex.speed
+
     layer.offsetX = ((width - layer.size) * vortex.offsetMultiplierX + layer.size / 2) + vortex.offset.x
-    layer.offsetY = ((width - layer.size) * vortex.offsetMultiplierY + layer.size / 2) + vortex.offset.y
+    layer.offsetY = ((height - layer.size) * vortex.offsetMultiplierY + layer.size / 2) + vortex.offset.y
   })
 
-  vortex.maxSize = maxSize
 }
 
 function updateStars(vortex) {
   _.forEach(vortex.stars, star => {
-    if (star.radius >= vortex.size) {
+    if (star.radius >= (vortex.size + 30)) {
       star.radius = random(vortex.minSize - (vortex.minSize * 0.6), vortex.minSize + (vortex.minSize / 2))
     } else {
-      star.radius += (vortex.speed * 1)
+      star.radius += (vortex.speed * 2.5)
     }
     star.position = createVector(star.radius * cos(star.theta), star.radius * sin(star.theta))
   })
@@ -142,7 +185,7 @@ function drawGrid(vortex) {
 
   blendMode(ADD)
 
-  let gridLines = 10 + (sin(frameCount * PI / 360) * 15 / 2)
+  let gridLines = 12 + (sin(frameCount * PI / animationSpeed) * 12 / 2)
   let vertexCount = 4
   let vertexTheta = TWO_PI / vertexCount
 
@@ -167,13 +210,13 @@ function drawGrid(vortex) {
       let yMax = (((vortex.size / 2) * y) / m) + (height / 2 - gridOriginY)
 
 
-      strokeWeight(0.75)
+      strokeWeight(0.5)
       stroke(255)
       line(xMin, yMin, xMax, yMax)
       stroke(vortex.c1)
-      line(xMin - 0.75, yMin - 0.75, xMax - 0.75, yMax - 0.75)
+      line(xMin - 0.5, yMin - 0.5, xMax - 0.5, yMax - 0.5)
       stroke(vortex.c2)
-      line(xMin + 0.75, yMin + 0.75, xMax + 0.75, yMax + 0.75)
+      line(xMin + 0.5, yMin + 0.5, xMax + 0.5, yMax + 0.5)
 
     })
 
@@ -183,6 +226,8 @@ function drawGrid(vortex) {
 }
 
 function drawLayer(vortex, layer, rot = false) {
+  if (layer.size < vortex.minSize) return
+
   push()
   translate(layer.offsetX, layer.offsetY)
   if(rot)
@@ -192,7 +237,7 @@ function drawLayer(vortex, layer, rot = false) {
   noFill()
   blendMode(ADD)
 
-  strokeWeight(1)
+  strokeWeight(0.5)
 
   layer.cR.setAlpha(map(layer.size, vortex.minSize, vortex.size, 100, 255))
   layer.cB.setAlpha(map(layer.size, vortex.minSize, vortex.size, 255, 100))
@@ -201,10 +246,10 @@ function drawLayer(vortex, layer, rot = false) {
   rect(0, 0, layer.size, layer.size)
 
   stroke(layer.cR)
-  rect(0, 0, layer.size + 2, layer.size + 2)
+  rect(0, 0, layer.size + 1, layer.size + 1)
 
   stroke(layer.cB)
-  rect(0, 0, layer.size - 2, layer.size - 2)
+  rect(0, 0, layer.size - 1, layer.size - 1)
 
   pop()
 }
@@ -221,12 +266,14 @@ function drawStars(vortex) {
   )
 
   _.forEach(vortex.stars, star => {
-    let st1 = 255 * abs(sin(frameCount * star.lightR * (PI / 360)))
-    let st2 = 255 * abs(sin(frameCount * star.lightB * (PI / 360)))
+    let st1 = 255 * abs(sin(star.lightR * (PI / animationSpeed)))
+    let st2 = 255 * abs(sin(star.lightB * (PI / animationSpeed)))
 
-    strokeWeight(2)
+    strokeWeight(1)
     star.cR.setAlpha(st1)
     star.cB.setAlpha(st2)
+    star.lightR += 1
+    star.lightB += 1
 
     stroke(star.cR)
     point(star.position.x, star.position.y)
